801fa0cd5299459b8fce646698f1de5e533d2fe5
jadefeng <j.feng.1991@gmail.com>
2014-11-04 23:19:41 +1100

    almost finished bucketlist

--- jade_feng/W9D2/bucketList-localstorage/js/bucketlist.js
+++ jade_feng/W9D2/bucketList-localstorage/js/bucketlist.js
@@ -1,3 +1,7 @@
+// TODO - output the showVisitedPlaces and showWishListPlaces to output each object onto the page
+// 		- save movements in objects from visited to wishlist and vice versa when a checkbox is clicked
+// 		- understand what the hell is going on with the window.localStorage
+
 var bucket = {
 	// countries: ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"],
 
@@ -26,58 +30,67 @@ var bucket = {
 
 	},
 
-	reset: function () {
-		localStorage.removeItem('bucket');
-		window.location.reload();
+	// reset: function () {
+	// 	localStorage.removeItem('bucket');
+	// 	window.location.reload();
+
+	// },
+
+	showVisitedPlaces: function () { // also need to move it around in the array to wishlist places
+		var visitedPlaces = $('.visited').empty();
+		_.each ( bucket.visited, function (entry) {
+			console.log(entry);
+		});
+		// _visited.each( function( $entryHTML ) {
+		// 	$('.places').append( $entryHTML );
+		// });
+	},
+
+	showWishListPlaces: function() {
+		var wishlistPlaces = $('.places').empty();
+		_.each ( bucket.wishlist, function (entry) {
+			console.log(entry);
+		});
+		
 
 	},
 
-	// showVisitedPlaces: function ($entryHTML) { // also need to move it around in the array to wishlist places
-	// 	var visitedPlaces = $('.visited').empty();
-	// 	_visited.each( function( $entryHTML ) {
-	// 		$('.places').append( $entryHTML );
-	// 	});
-	// },
-
-	// showWishListPlaces: function($entryHTML) {
-	// 	var visitedPlaces = $('.places').empty();
-	// 	_visited.each( function( $entryHTML ) {
-	// 		$('.places').append( $entryHTML );
-	// 	});
-	// },
-
 	enterCountry: function(place, score, date, image, completed) {
 		// console.log( place, score, date, image, completed);
-		var entryHtml = '<div class="entry_box">';
-		var text = '<h3>' + place + '</h3>';
-		var img = '<img class="image" src="' + image + '" />';
-		var score = '<h4>' + score + '</h4>';
-		var date = '<h4>' + date + '</h4>';
-		var checkbox = '<input type="checkbox" class="visited_now">';
-		var deletebox = '<button class="delete"> Delete </button>'
-		var closing = '</div>';
-		var $entryHTML = entryHtml + text +img +score + date + checkbox + deletebox + closing;
+		// var entryHtml = '<div class="entry_box">';
+		// var text = '<h3>' + place + '</h3>';
+		// var img = '<img class="image" src="' + image + '" />';
+		// var score = '<h4>' + score + '</h4>';
+		// var date = '<h4>' + date + '</h4>';
+		// var checkbox = '<input type="checkbox" class="visited_now">';
+		// var deletebox = '<button class="delete"> Delete </button>';
+		// var closing = '</div>';
+		// var $entryHTML = entryHtml + text +img +score + date + checkbox + deletebox + closing;
 
 		if (completed) {
 			// bucket.showVisitedPlaces( $entryHTML ); // Need to save this into the showVisitedPlaces and save to database
-			$('.visited').html($entryHTML); // Need a function called showVisited instead
-			visited.push({
+			// $('.visited').append($entryHTML); // Need a function called showVisited instead
+			bucket.visited.push({
 				place: place,
 				score: score,
 				date: date,
 				image: image,
 			});
+
+			bucket.showVisitedPlaces();
 
 		} else {
 			// bucket.showWishListPlaces( $entryHTML ); // Need to save this into the shownon-visited and save to database
-			$('.places').html($entryHTML); // need a new function called showWishList
+			// $('.places').append($entryHTML); // need a new function called showWishList
 
-			wishlist.push({
+			bucket.wishlist.push({
 				place: place,
 				score: score,
 				date: date,
 				image: image,
 			});
+
+			bucket.showWishListPlaces();
 
 		}
 
